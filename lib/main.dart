b9f31365bb856cf77f2e977cac97ffab3f3ed3ea
Amangi-Jnr <sundayamangi@gmail.com>
2026-01-25 18:51:58 +0100

    new user group added, email pattern changed, bug fix for verifcation emails, some ui changes

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'NileAssist',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         progressIndicatorTheme: const ProgressIndicatorThemeData(
